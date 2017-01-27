33459ae2ca690af7b2882dbecf0dfa8e3cabcd4e
Ryan Gross <rgross@udel.edu>
2017-01-26 22:18:22 -0500

    Fixed github issue

--- src/main/java/ginzberg/casino/Ranks.java
+++
@@ -1,6 +0,0 @@
-package ginzberg.casino;
-
-/**
- * Created by williammattern on 1/24/17.
- */
-public enum Ranks {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
--- src/main/java/ginzberg/casino/Suits.java
+++
@@ -1,6 +0,0 @@
-package ginzberg.casino;
-
-/**
- * Created by williammattern on 1/24/17.
- */
-public enum Suits {SPADES, HEARTS, CLUBS, DIAMONDS}
